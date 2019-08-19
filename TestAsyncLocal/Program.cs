002a9d41ab4caf2c9cc92cfe211ba4f6c60b1f4e
Cooler Zhang 🐾 <cooler.zhang@serko.com>
2019-08-19 11:24:42 +0800

    Init Project

---
+++ TestAsyncLocal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace TestAsyncLocal
+{
+    internal class Program
+    {
+        private static readonly AsyncLocal<TestDbContext> AsyncTestDbContext = new AsyncLocal<TestDbContext>();
+
+        private static void Main(string[] args)
+        {
+            GetDbContext();
+            SetDbContext();
+            GetDbContext();
+            Thread.Sleep(3000);
+            GetDbContext();
+            Console.ReadLine();
+        }
+
+        public static void SetDbContext()
+        {
+            AsyncTestDbContext.Value = new TestDbContext();
+        }
+
+        public static void GetDbContext()
+        {
+            var dbContext = AsyncTestDbContext.Value;
+            Console.WriteLine("CurrentDbContext:" + dbContext?.Id);
+        }
+    }
+
+    public class TestDbContext
+    {
+        public string Id { get; set; }
+
+        public TestDbContext()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
